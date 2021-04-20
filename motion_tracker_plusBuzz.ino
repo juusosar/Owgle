53e0b1c59ff6795cad1fc3cbaf90bb132a9c8d19
juusosar <71127114+juusosar@users.noreply.github.com>
2021-04-20 12:12:00 +0300

    Add files via upload

---
+++ motion_tracker_plusBuzz.ino
@@ -0,0 +1,107 @@
+#include <Servo.h>
+Servo myservo;
+int sensor_1_trig = 13;
+int sensor_1_echo = 12;
+
+int sensor_2_trig = 11;
+int sensor_2_echo = 10;
+
+int servo = 2;
+
+int buzzer = 9;
+
+long duration_right, duration_left, cm_right, cm_left;
+int threshold = 50;
+int angle = 90;
+int shout = 3;
+
+
+void setup() {
+  Serial.begin(9600);
+  myservo.attach(2);
+  
+  pinMode(sensor_1_trig, OUTPUT);
+  pinMode(sensor_1_echo, INPUT);
+
+  pinMode(sensor_2_trig, OUTPUT);
+  pinMode(sensor_2_echo, INPUT);
+
+  pinMode(buzzer, OUTPUT);
+
+}
+
+void loop() {
+
+  digitalWrite(sensor_1_trig, LOW);
+  delayMicroseconds(5);
+  digitalWrite(sensor_1_trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(sensor_1_trig, LOW);
+
+  duration_right = pulseIn(sensor_1_echo, HIGH);
+
+  cm_right = (duration_right/2) * 0.0343;
+
+  Serial.print(cm_right);
+  Serial.print("cm_right");
+  Serial.println();
+
+  delay(5);
+
+  digitalWrite(sensor_2_trig, LOW);
+  delayMicroseconds(5);
+  digitalWrite(sensor_2_trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(sensor_2_trig, LOW);
+
+  duration_left = pulseIn(sensor_2_echo, HIGH);
+
+  cm_left = (duration_left/2) * 0.0343;
+
+  Serial.print(cm_left);
+  Serial.print("cm_left");
+  Serial.println();
+
+
+  follow();
+  
+  buzz();
+  
+  delay(5);
+  
+}
+
+void follow() 
+{
+  if (cm_left <= threshold || cm_right <= threshold)
+  {
+    if (cm_left + 2 < cm_right) 
+    {
+      angle = angle + 2;
+    }
+    if (cm_right + 2 < cm_left)
+    {
+      angle = angle - 2;
+    }
+  }
+  if (angle > 160)
+  {
+    angle = 160;
+  }
+  if (angle < 0)
+  {
+    angle = 0;
+  }
+  myservo.write(angle);
+}
+
+void buzz()
+{
+  if (cm_right <= shout || cm_left <= shout)
+  {
+    tone(buzzer, 750);
+    delay(1000);
+    noTone(buzzer);
+    angle = 0;
+  }
+}
